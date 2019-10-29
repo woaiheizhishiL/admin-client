e61e17bb84659186b74a0ac222fc7befb831af34
woaiheizhishiL <690221567@qq.com>
2019-10-30 00:30:52 +0800

    06_Admin的整体结构

---
+++ src/containers/admin/header/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './index.less'
+
+export default class AdminHeader extends Component {
+    render() {
+        return (
+            <div className="header">
+                Header
+            </div>
+        )
+    }
+}
--- src/containers/admin/index.jsx
+++ src/containers/admin/index.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
+// import {connect} from 'react-redux'
+// import {Redirect} from 'react-router-dom'
+import { Layout } from 'antd';
 
-import { removeUserToken } from '../../redux/action-creators/user'
-import {reqUsers} from '../../api'
+// import { removeUserToken } from '../../redux/action-creators/user'
+// import {reqUsers} from '../../api'
+import LeftNav from './left-nav'
+import AdminHeader from './header'
 import WithCheckLogin from '../with-check-login'
+
+
+const { Header, Footer, Sider, Content } = Layout;
+
 
 /* connect(
   state => ({user:state.user.user,hasLogin:state.user.hasLogin}),
@@ -12,13 +19,14 @@ import WithCheckLogin from '../with-check-login'
 )(WithCheckLogin(Admin)) 
  */
 
-@connect(
+/* @connect(
   state => ({user:state.user.user}),
   {removeUserToken}
-) 
+)  */
 @WithCheckLogin
 class Admin extends Component {
-  logout = () => {
+
+  /* logout = () => {
     this.props.removeUserToken()
   }
 
@@ -26,19 +34,32 @@ class Admin extends Component {
     reqUsers()
     const result = await reqUsers()
     console.log('result',result)
-  }
+  } */
 
   render() {
-    // if (!this.props.hasLogin){
-    //   return <Redirect to="/login"/>
-    // }    
+    /* if (!this.props.hasLogin){
+      return <Redirect to="/login"/>
+    }  */   
 
     return (
-      <div>
+      /* <div>
        <p>Hello,{this.props.user.username}</p>
        <button onClick={this.logout}>退出登录</button>
        <button onClick={this.getUsers}>获取用户列表</button>
-      </div>
+      </div> */
+
+      <Layout style={{height:"100%"}}>
+        <Sider >
+          <LeftNav/>
+        </Sider>
+        <Layout>
+            <AdminHeader/>
+          <Content style={{backgroundColor:"white",margin:"30px 15px 0 15px"}}>
+            二级路由组件界面
+          </Content>
+          <Footer style={{textAlign:"center"}}>推荐使用谷歌浏览器，可以活得更加页面操作体验</Footer>
+        </Layout>
+      </Layout>
     )
   }
 }
