8cca2f9a320964b36d8c2bd81f350e8f74ba8c35
woaiheizhishiL <690221567@qq.com>
2019-10-30 09:48:16 +0800

    07_Admin的子路由搭建

--- src/containers/admin/index.jsx
+++ src/containers/admin/index.jsx
@@ -2,12 +2,22 @@ import React, {Component} from 'react'
 // import {connect} from 'react-redux'
 // import {Redirect} from 'react-router-dom'
 import { Layout } from 'antd';
+import {Route,Switch,Redirect} from 'react-router-dom'
 
 // import { removeUserToken } from '../../redux/action-creators/user'
 // import {reqUsers} from '../../api'
 import LeftNav from './left-nav'
 import AdminHeader from './header'
 import WithCheckLogin from '../with-check-login'
+import Home from '../../components/home'
+import Category from '../category'
+import Product from '../products'
+import Role from '../role'
+import User from '../user'
+import Line from '../../components/charts/line'
+import Bar from '../../components/charts/bar'
+import Pie from '../../components/charts/pie'
+
 
 
 const { Header, Footer, Sider, Content } = Layout;
@@ -55,7 +65,17 @@ class Admin extends Component {
         <Layout>
             <AdminHeader/>
           <Content style={{backgroundColor:"white",margin:"30px 15px 0 15px"}}>
-            二级路由组件界面
+            <Switch>
+              <Route path="/home" component={Home}/>
+              <Route path="/category" component={Category}/>
+              <Route path="/product" component={Product}/>
+              <Route path="/role" component={Role}/>
+              <Route path="/user" component={User}/>
+              <Route path="/charts/line" component={Line}/>
+              <Route path="/charts/bar" component={Bar}/>
+              <Route path="/charts/pie" component={Pie}/>
+              <Redirect to="/home"/>
+            </Switch>
           </Content>
           <Footer style={{textAlign:"center"}}>推荐使用谷歌浏览器，可以活得更加页面操作体验</Footer>
         </Layout>
