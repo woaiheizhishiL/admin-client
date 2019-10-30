123bb4bcdfeb48cbcf3690e80e000d829bcad57a
woaiheizhishiL <690221567@qq.com>
2019-10-30 11:18:08 +0800

    08_Header组件_界面_LinkButton组件

--- src/containers/admin/header/index.jsx
+++ src/containers/admin/header/index.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import './index.less'
+import LinkButton from '../../../components/link-button'
 
 export default class AdminHeader extends Component {
+
+    logout = () => {
+        alert('logout')
+    }
     render() {
         return (
             <div className="header">
-                Header
+                <div className="header-top">
+                    <span>欢迎，xxx</span>
+                    <LinkButton onClick={this.logout}>退出</LinkButton>
+                </div> 
+                <div className="header-bottom">
+                    <div className="header-bottom-left">折线图</div>
+                    <div className="header-bottom-right">
+                        <span>时间</span>
+                        <img src="" alt="weather"/>
+                        <span>小雨转多云</span>
+                    </div>
+                </div>
             </div>
         )
     }
